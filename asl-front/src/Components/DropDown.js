0b4be300a2cc12dae39a999cc1ceb3759b6d9043
martinacruz <cruz.s.martin@gmail.com>
2021-11-03 20:04:40 -0700

    drop down menu for searches fixed and no long changing height of nav bar

--- asl-front/src/Components/DropDown.js
+++ asl-front/src/Components/DropDown.js
@@ -5,7 +5,7 @@ const DropDown = ({ emptySearch, searchResults}) => {
     return emptySearch ? null : <div className="results-container">
         {searchResults.slice(0, 5).map(show => (
             <div className="show-container" key={show.mal_id}>
-            <img src={show.image_url} alt="" className="results-show-img" />
+            <img src={show.image_url} alt="show image" className="results-show-img" />
             <div className="restuls-show-text">
             <p className='results-show-title'>{show.title}</p>
             <p className='results-show-data'>{`${show.episodes}epi | ${new Date(show.start_date).toISOString().split('T')[0]} | ${show.score} | ${show. airing == true ? "Ongoing" : "Completed" }`}</p>
